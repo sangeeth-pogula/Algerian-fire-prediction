55851288925f9eab645b58a9ae14f0a28fb464ed
POGULA SANGEETH <sangeeth.pogula@gmail.com>
2024-09-12 13:58:54 +0530

    modify the index route to render the index.html

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ app = application
 
 @app.route("/", methods=["GET"])
 def index():
-    return "<h1>Hello World</h1>"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug="True")
