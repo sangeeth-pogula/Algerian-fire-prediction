16af5a1642d27b49e8730b1fff34ed965dfcb6fa
POGULA SANGEETH <sangeeth.pogula@gmail.com>
2024-09-12 14:08:06 +0530

    Import necessary imports and Configure the logger in dependencies.py

---
+++ dependencies.py
@@ -0,0 +1,27 @@
+import pickle
+import pandas as pd
+import numpy as np
+import json
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+# Configure logging
+log_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+log_file = 'app.log'
+
+# Set up file handler with rotation
+file_handler = RotatingFileHandler(log_file, maxBytes=10*1024*1024, backupCount=5)
+file_handler.setFormatter(log_formatter)
+file_handler.setLevel(logging.INFO)
+
+# Set up console handler
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(log_formatter)
+console_handler.setLevel(logging.INFO)
+
+# Get the root logger and configure handlers
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
