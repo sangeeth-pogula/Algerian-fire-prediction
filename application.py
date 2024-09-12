3fc2137a5a8276b8933410935a70926b45e7794e
POGULA SANGEETH <sangeeth.pogula@gmail.com>
2024-09-12 14:00:15 +0530

    Add a predict route in application.py

--- application.py
+++ application.py
@@ -7,5 +7,9 @@ app = application
 def index():
     return render_template('index.html')
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    pass
+
 if __name__ == "__main__":
     app.run(debug="True")
